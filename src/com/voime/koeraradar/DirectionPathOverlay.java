0ad88ffd2f83c5742f90ec35c370d333fc16e9da
Arvi Võime <avoime@gmail.com>
2012-09-05 22:38:34 +0300

    bugfix

---
+++ src/com/voime/koeraradar/DirectionPathOverlay.java
@@ -0,0 +1,50 @@
+package com.voime.koeraradar;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
+class DirectionPathOverlay extends Overlay {
+
+    private GeoPoint gp1;
+    private GeoPoint gp2;
+
+    public DirectionPathOverlay(GeoPoint gp1, GeoPoint gp2) {
+        this.gp1 = gp1;
+        this.gp2 = gp2;
+    }
+
+    @Override
+    public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
+            long when) {
+        // TODO Auto-generated method stub
+        com.google.android.maps.Projection projection = mapView.getProjection();
+        if (shadow == false) {
+
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            Point point = new Point();
+            projection.toPixels(gp1, point);
+            paint.setColor(Color.RED);
+            Point point2 = new Point();
+            projection.toPixels(gp2, point2);
+            paint.setStrokeWidth(1);
+            canvas.drawLine((float) point.x, (float) point.y, (float) point2.x,(float) point2.y, paint);
+            canvas.drawCircle((float) point2.x, (float) point2.y, 3, paint);
+        }
+        return super.draw(canvas, mapView, shadow, when);
+    }
+
+    @Override
+    public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+        // TODO Auto-generated method stub
+
+        super.draw(canvas, mapView, shadow);
+    }
+
+}
